6a581c084112b1e74f7cd4c518d033a021addcfa
Dyonata Machado <dyonatamachado95@gmail.com>
2021-06-03 10:14:22 -0300

    Troquei lógica que tinha feito pelo método Array.Sort

--- Triangulos/Program.cs
+++ Triangulos/Program.cs
@@ -7,116 +7,23 @@ namespace Triangulos
         static void Main(string[] args)
         {
             string[] s = Console.ReadLine().Split(' ');
+            Array.Sort(s);
             double a = double.Parse(s[0]);
             double b = double.Parse(s[1]);
             double c = double.Parse(s[2]);
 
-            double ladoa = 0;
-            double ladob = 0;
-            double ladoc = 0;
 
-            if((a != b && b != c) && a != c)
-            {
-                if(a > b && a > c)
-                {
-                    if(b > c)
-                    {
-                        ladoa = a;
-                        ladob = b;
-                        ladoc = c;
-                    } else
-                    {
-                        ladoa = a;
-                        ladob = c;
-                        ladoc = b;
-                    }
-                } else if (a > b && a < c)
-                {
-                    ladoa = c;
-                    ladob = a;
-                    ladoc = b;
-                } else if (a < b && a < c)
-                {
-                    if(b < c)
-                    {
-                        ladoa = c;
-                        ladob = b;
-                        ladoc = a;
-                    }
-                    else
-                    {
-                        ladoa = b;
-                        ladob = c;
-                        ladoc = a;
-                    }
-                } else if(a < b && a > c)
-                {
-                    ladoa = b;
-                    ladob = a;
-                    ladoc = c;
-                }
-
-            } else
-            {
-                if(a == b)
-                {
-                   if(a > c)
-                    {
-                        ladoa = a;
-                        ladob = b;
-                        ladoc = c;
-                    } else
-                    {
-                        ladoa = c;
-                        ladob = a;
-                        ladoc = b;
-                    }
-                } else if (a == c)
-                {
-                    if (a > b)
-                    {
-                        ladoa = a;
-                        ladob = c;
-                        ladoc = b;
-                    } else
-                    {
-                        ladoa = b;
-                        ladob = a;
-                        ladoc = c;
-                    }
-                } else if (b == c) 
-                { 
-                    if(a > b)
-                    {
-                        ladoa = a;
-                        ladob = b;
-                        ladoc = c;
-                    }
-                    else
-                    {
-                        ladoa = b;
-                        ladob = c;
-                        ladoc = a;
-                    }
-                } else if (a == b && a == c)
-                {
-                    ladoa = a;
-                    ladob = b;
-                    ladoc = c;
-                }
-            }
-
-            if (ladoa >= (ladob + ladoc))
+            if (a >= (b + c))
                 Console.WriteLine("NAO FORMA TRIANGULO");
-            else if ((ladoa*ladoa) == (ladob*ladob) + (ladoc*ladoc))
+            else if ((a * a) == (b * b) + (c * c))
                 Console.WriteLine("TRIANGULO RETANGULO");
-            else if ((ladoa * ladoa) > (ladob * ladob) + (ladoc * ladoc))
+            else if ((a * a) > (b * b) + (c * c))
                 Console.WriteLine("TRIANGULO OBTUSANGULO");
-            else if ((ladoa * ladoa) < (ladob * ladob) + (ladoc * ladoc))
+            else if ((a * a) < (b * b) + (c * c))
                 Console.WriteLine("TRIANGULO ACUTANGULO");
-            if ((ladoa == ladob) && (ladoa == ladoc))
+            if ((a == b) && (a == c))
                 Console.WriteLine("TRIANGULO EQUILATERO");
-            if ((((ladoa == ladob) && (ladoa != ladoc)) || ((ladoc == ladob) && (ladoa != ladoc))) || ((ladoa == ladoc) && (ladoa != ladob)))
+            if ((((a == b) && (a != c)) || ((c == b) && (a != c))) || ((a == c) && (a != b)))
                 Console.WriteLine("TRIANGULO ISOSCELES");
 
             Console.ReadLine();
